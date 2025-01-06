6af8f5843b0c83eaedac156f11c50347296e39ee
poulakise <evanspoulaki@gmail.com>
2025-01-06 03:15:30 +0200

    feat(ProducerDemo): new class ProducerDemo.

---
+++ src/main/java/gr/evansp/ProducerDemo.java
@@ -0,0 +1,19 @@
+package gr.evansp;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ProducerDemo {
+	/**
+	 * {@link Logger}.
+	 */
+	private static final Logger logger = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+
+	/**
+	 * Main
+	 * @param args args
+	 */
+	public static void main(String... args) {
+		logger.info("lalala");
+	}
+}
