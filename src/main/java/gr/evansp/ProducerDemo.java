9c23fa8f19fe980e3abb91480e47a459cf5f9d6f
poulakise <evanspoulaki@gmail.com>
2025-01-06 21:54:26 +0200

    feat(ProducerDemo): ProducerDemo send and send with callback.

--- src/main/java/gr/evansp/ProducerDemo.java
+++ src/main/java/gr/evansp/ProducerDemo.java
@@ -1,8 +1,18 @@
 package gr.evansp;
 
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * {@link ProducerDemo}
+ */
+@SuppressWarnings("nls")
 public class ProducerDemo {
 	/**
 	 * {@link Logger}.
@@ -10,10 +20,63 @@ public class ProducerDemo {
 	private static final Logger logger = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
 
 	/**
+	 * BOOTSTRAP_SERVER
+	 */
+	public static final String BOOTSTRAP_SERVER = "localhost:9093";
+
+	/**
+	 * TOPIC
+	 */
+	public static final String TOPIC = "java_topic";
+
+	/**
 	 * Main
 	 * @param args args
 	 */
 	public static void main(String... args) {
-		logger.info("lalala");
+		sendRecord();
+		sendRecordWithCallback();
+	}
+
+	private static void sendRecordWithCallback() {
+		ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC, "hello World");
+
+		Properties properties = connectionProperties();
+
+		try(KafkaProducer<String, String> producer = new KafkaProducer<>(properties)) {
+			producer.send(record, (recordMetadata, e) -> {
+				if (e == null) {
+					logger.info("record successfully send");
+				} else {
+					logger.error(e.getMessage());
+				}
+			});
+			producer.flush();
+		}
+	}
+
+
+	private static void sendRecord() {
+		ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC, "hello World");
+
+		Properties properties = connectionProperties();
+
+		try(KafkaProducer<String, String> producer = new KafkaProducer<>(properties)) {
+			producer.send(record);
+			producer.flush();
+		}
+	}
+
+	/**
+	 * Returns the required configuration to connect to a kafka bootstrap server INSECURELY.
+	 *
+	 * @return Properties
+	 */
+	private static Properties connectionProperties() {
+		Properties kafkaProperties = new Properties();
+		kafkaProperties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVER);
+		kafkaProperties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		kafkaProperties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		return kafkaProperties;
 	}
 }
